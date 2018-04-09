31a91089c74694fd39f59302338de58e523ac5e1
agtokty <agitoktay@gmail.com>
2018-04-09 15:55:03 +0300

    add i18n

--- controllers/api.js
+++ controllers/api.js
@@ -7,4 +7,15 @@ router.get('/users', function (req, res, next) {
     res.send(database.getAll());
 });
 
+router.get('/i18n', function (req, res, next) {
+    res.send({
+        "en": {
+            translation: {
+                "login-app-title": "Welcome to " + (process.env.TITLE || "NetCet v0.2"),
+                "chat-app-title": (process.env.TITLE || "NetCet v0.2"),
+            }
+        },
+    });
+});
+
 module.exports = router;
